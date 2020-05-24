bff5be9c35341d268a3a0e5ea33253c5047c8c2d
kolweb-dev <kolya.tkachukki@gmail.com>
2020-05-24 15:43:30 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(function () {
+
+    $(".rate-start").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    $('.product-slider_inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
